f4896249022349fd4714c761107b3565a5e247cb
irooori <168460203+irooori@users.noreply.github.com>
2024-10-20 17:54:10 +0200

    Update using VEML6040 library

--- example/colour_detection/colour_detection/colour_detection.ino
+++ example/colour_detection/colour_detection/colour_detection.ino
@@ -1,33 +1,51 @@
 #include <Dezibot.h>
 
+#include "Wire.h"
+#include "veml6040.h"
+
 Dezibot dezibot = Dezibot();
+
+VEML6040 RGBWSensor;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Started");
   dezibot.begin();
   Serial.println("Initialised");
+
+  if(!RGBWSensor.begin()) {
+    Serial.println("ERROR: couldn't detect the sensor");
+    while(1){}
+  }
+  RGBWSensor.setConfiguration(VEML6040_IT_320MS + VEML6040_AF_AUTO + VEML6040_SD_ENABLE);
+	
+  delay(1500);
+  Serial.println("CCT: Correlated color temperature in \260K");
+  Serial.println("AL: Ambient light in lux");
+  delay(1500);
 }
 
 void loop() {
-  printValue(VEML_RED, "R");
-  printValue(VEML_GREEN, "G");
-  printValue(VEML_BLUE, "B");
-  printValue(VEML_WHITE, "W");
+  printValue(RGBWSensor.getRed(), "R");
+  printValue(RGBWSensor.getGreen(), "G");
+  printValue(RGBWSensor.getBlue(), "B");
+  printValue(RGBWSensor.getWhite(), "W");
+  Serial.print(" CCT: ");
+  Serial.print(RGBWSensor.getCCT());  
+  Serial.print(" AL: ");
+  Serial.println(RGBWSensor.getAmbientLight()); 
+  delay(400);
 
-  delay(500);
   Serial.println("");
   dezibot.display.clear();
 }
 
-void printValue(color color, char* prefix) {
-  uint16_t colorValue = dezibot.colorDetection.getColorValue(color);
-
+void printValue(uint16_t color, char* prefix) {
   dezibot.display.print(prefix);
   dezibot.display.print(" ");
-  dezibot.display.println(colorValue);
+  dezibot.display.println(color);
 
   Serial.print(prefix);
   Serial.print(" ");
-  Serial.println(colorValue);
+  Serial.println(color);
 }
