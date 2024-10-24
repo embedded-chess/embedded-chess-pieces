3d88e8a99e3c4692daf031c5ff7ccc4cdd82eb09
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-10-24 09:59:44 +0200

    Update doc strings.

--- example/colour_detection/colour_detection/colour_detection.ino
+++ example/colour_detection/colour_detection/colour_detection.ino
@@ -1,3 +1,14 @@
+/**
+ * @file colour_detection.ino
+ * @author Ines Rohrbach, Nico Schramm
+ * @brief Example to determine if Dezibot is on white or black surface
+ * @version 0.1
+ * @date 2024-10-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include <Dezibot.h>
 #include <Wire.h>
 #include <veml6040.h>
@@ -51,7 +62,11 @@ void loop() {
   dezibot.display.clear();
 }
 
-// Get normalized value of ambient light sensor on a scale from 0 to 255.
+/**
+ * @brief Get normalized value of ambient light sensor on a scale from 0 to 255.
+ * 
+ * @return double normalized value
+ */
 double getNormalizedAmbientValue() {
   const double MAX_AMBIENT_VALUE = 2061.0;
 
@@ -61,7 +76,15 @@ double getNormalizedAmbientValue() {
   return normalizedValue;
 }
 
-// Get raw color value of VEML sensor (**not** normalized!).
+/**
+ * @brief Get raw color value of RGBW sensor (**not** normalized!).
+ *
+ * Raw value is scaled on 16 bits.
+ * For normalized value see \p getNormalizedColorValue.
+ * 
+ * @param color color which to get
+ * @return double color value
+ */
 double getRawColorValue(color color) {
   switch (color) {
     case VEML_RED:
@@ -75,8 +98,16 @@ double getRawColorValue(color color) {
   }
 }
 
-// Get normalize value of VEML color sensor on a scale of 0 to 255 based on
-// ambient lighting conditions.
+/**
+ * @brief Get the normalized value of color sensor on a scale of 0 to 255 based
+ *        on ambient lighting conditions.
+ *
+ * For \p ambientLightValue please use \p getNormalizedColorValue.
+ * 
+ * @param color color which to get value for
+ * @param ambientLightValue **normalized** value of ambient light sensor
+ * @return double normalized color value
+ */
 double getNormalizedColorValue(color color, double ambientLightValue) {
   const double MAX_SENSOR_VALUE = 65535.0;
 
@@ -86,9 +117,19 @@ double getNormalizedColorValue(color color, double ambientLightValue) {
   return normalizedValue;
 }
 
-// Calculate brightness value based on **normalized** RGB values.
+/**
+ * @brief Approximate brightness value based on **normalized** RGB values.
+ * 
+ * This method uses the approximation algorithm proposed in
+ * https://www.w3.org/TR/AERT/#color-contrast.
+ * The approximated value is normalized to a scale of 0 to 255.
+ * 
+ * @param red normalized red value
+ * @param green normalized green value
+ * @param blue normalized blue value
+ * @return double approximation for color value
+ */
 double calculateBrightness(double red, double green, double blue) {
-  // see https://www.w3.org/TR/AERT/#color-contrast
   const double RED_FACTOR = 0.299;
   const double GREEN_FACTOR = 0.587;
   const double BLUE_FACTOR = 0.114;
@@ -98,14 +139,29 @@ double calculateBrightness(double red, double green, double blue) {
   return normalizedBrightness;
 }
 
-// Determine if brightness value represents a white or a black chess field.
-// Note that the room must be well-lit.
+/**
+ * @brief Determine if brightness value represents a white or a black chess
+ *        field.
+ *
+ * Note that the room must be well-lit!
+ * 
+ * @param brightness normalized brightness value
+ * @return true if surface is white-ish
+ * @return false if surface is black-ish
+ */
 bool isWhiteField(double brightness) {
   const double THRESHOLD = 100.0;
   return brightness >= THRESHOLD;
 }
 
-// Print passed color value to dezibot display and serial monitor.
+/**
+ * @brief Print passed color value to dezibot display and serial monitor.
+ * 
+ * For example, `printValue(100, "R")` will result in `R 100`.
+ * 
+ * @param colorValue value of color to print
+ * @param prefix prefix of color value to print, e.g. "R" for red
+ */
 void printValue(double colorValue, char* prefix) {
   dezibot.display.print(prefix);
   dezibot.display.print(" ");
@@ -116,7 +172,19 @@ void printValue(double colorValue, char* prefix) {
   Serial.println(colorValue);
 }
 
-// Print formatted info for isWhiteField.
+/**
+ * @brief Print formatted info for isWhiteField.
+ * 
+ * \code{.cpp}
+ * printIsWhiteField(true)
+ * // "F W"
+ * 
+ * printIsWhiteField(false)
+ * \endcode
+ * ```
+ * 
+ * @param isWhiteField true if is white, false otherwise
+ */
 void printIsWhiteField(bool isWhiteField) {
   dezibot.display.print("F ");
   Serial.print("F ");
