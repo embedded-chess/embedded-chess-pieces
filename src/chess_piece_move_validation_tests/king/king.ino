146aa12167237a60d607a7e95a9c8f70d7bc3f38
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-11-27 16:15:43 +0100

    Update test case formatting.

--- src/chess_piece_move_validation_tests/king/king.ino
+++ src/chess_piece_move_validation_tests/king/king.ino
@@ -41,17 +41,28 @@ void testKing() {
 
     // valid moves
     Serial.println("Testing valid moves...");
-    ECPChessField validFields[] = {{ E, 6}, { F, 6}, { G, 6}, { G, 5}, { G, 4},
-        { F, 4}, { E, 4}, { E, 5}};
+    ECPChessField validFields[] = {
+        { E, 6 }, { F, 6 }, { G, 6 },
+        { E, 5 }, /* fig */ { G, 5 },
+        { E, 4 }, { F, 4 }, { G, 4 },
+    };
     for (ECPChessField field : validFields) {
         test(king, field, true);
     }
 
     // invalid moves
     Serial.println("\nTesting invalid moves...");
-    ECPChessField invalidFields[] = {initialField, { D, 7}, { E, 7}, { F, 7},
-        { G, 7}, { H, 7}, { H, 6}, { H, 5}, { H, 4}, { H, 3}, { G, 3}, { F, 3},
-        { E, 3}, { D, 3}, { D, 4}, { D, 5}, { D, 6}};
+    ECPChessField invalidFields[] = {
+        initialField,
+        { A, 8 }, { B, 8 }, { C, 8 }, { D, 8 }, { E, 8 }, { F, 8 }, { G, 8 }, { H, 8 },
+        { A, 7 }, { B, 7 }, { C, 7 }, { D, 7 }, { E, 7 }, { F, 7 }, { G, 7 }, { H, 7 },
+        { A, 6 }, { B, 6 }, { C, 6 }, { D, 6 },                               { H, 6 },
+        { A, 5 }, { B, 5 }, { C, 5 }, { D, 5 },           /* fig */           { H, 5 },
+        { A, 4 }, { B, 4 }, { C, 4 }, { D, 4 },                               { H, 4 },
+        { A, 3 }, { B, 3 }, { C, 3 }, { D, 3 }, { E, 3 }, { F, 3 }, { G, 3 }, { H, 3 },
+        { A, 2 }, { B, 2 }, { C, 2 }, { D, 2 }, { E, 2 }, { F, 2 }, { G, 2 }, { H, 2 },
+        { A, 1 }, { B, 1 }, { C, 1 }, { D, 1 }, { E, 1 }, { F, 1 }, { G, 1 }, { H, 1 }
+    };
     for (ECPChessField field : invalidFields) {
         test(king, field, false);
     }
--- src/chess_piece_move_validation_tests/knight/knight.ino
+++ src/chess_piece_move_validation_tests/knight/knight.ino
@@ -41,8 +41,12 @@ void testKnight() {
 
     // valid moves
     Serial.println("Testing valid moves...");
-    ECPChessField validFields[] = {{ C, 7 }, { E, 7 }, { F, 6 }, { F, 4 },
-        { E, 3 }, { C, 3 }, { B, 4}, { B, 6 }};
+    ECPChessField validFields[] = {
+                  { C, 7 },           { E, 7 },
+        { B, 6 },                               { F, 6 },
+                            /* fig */
+        { B, 4 },                               { F, 4 },
+                  { C, 3 },           { E, 3 }};
     for (ECPChessField field : validFields) {
         test(knight, field, true);
     }
@@ -51,16 +55,14 @@ void testKnight() {
     Serial.println("\nTesting invalid moves...");
     ECPChessField invalidFields[] = {
         initialField,
-        // directly surrounding fields
-        { C, 6}, { D, 6 }, { E, 6 }, { E, 5 }, { E, 4 }, { D, 4}, { C, 4}, { C, 5 },
-        // surrounding + 1
-        { B, 7 }, { D, 7 }, { F, 7 }, { F, 5}, { F, 3 }, { D, 3 }, { B, 3 }, { B, 5 },
-        // surrounding + 2
-        { A, 8 }, { B, 8 }, { C, 8 }, { D, 8 }, { E, 8 }, { F, 8 }, { G, 8 },
-        { G, 7 }, { G, 6 }, { G, 5 }, { G, 4 }, { G, 3 }, { G, 2 },
-        { F, 2 }, { E, 2 }, { D, 2 }, { C, 2 }, { B, 2 }, { A, 2},
-        { A, 3 }, { A, 4 }, { A, 5 }, { A, 6 }, { A, 7 }
-    };
+        { A, 8 }, { B, 8 }, { C, 8 }, { D, 8 }, { E, 8 }, { F, 8 }, { G, 8 }, { H, 8 },
+        { A, 7 }, { B, 7 },           { D, 7 },           { F, 7 }, { G, 7 }, { H, 7 },
+        { A, 6 },           { C, 6 }, { D, 6 }, { E, 6 },           { G, 6 }, { H, 6 },
+        { A, 5 }, { B, 5 }, { C, 5 }, /* fig */ { E, 5 }, { F, 5 }, { G, 5 }, { H, 5 },
+        { A, 4 },           { C, 4 }, { D, 4 }, { E, 4 },           { G, 4 }, { H, 4 },
+        { A, 3 }, { B, 3 },           { D, 3 },           { F, 3 }, { G, 3 }, { H, 3 },
+        { A, 2 }, { B, 2 }, { C, 2 }, { D, 2 }, { E, 2 }, { F, 2 }, { G, 2 }, { H, 2 },
+        { A, 1 }, { B, 1 }, { C, 1 }, { D, 1 }, { E, 1 }, { F, 1 }, { G, 1 }, { H, 1 }};
     for (ECPChessField field : invalidFields) {
         test(knight, field, false);
     }
