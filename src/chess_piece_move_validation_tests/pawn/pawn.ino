fa4b45efb051c8bbf4a4455803feb45034c6ecd5
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-11-29 15:57:21 +0100

    Update test case formatting for pawn and rook.

--- src/chess_piece_move_validation_tests/pawn/pawn.ino
+++ src/chess_piece_move_validation_tests/pawn/pawn.ino
@@ -30,6 +30,8 @@ void loop() {
     testWhitePawn();
     Serial.println("\nTesting black pawn...");
     testBlackPawn();
+    Serial.println("\nTesting board center...");
+    testPawnInBoardCenterCanOnlyMoveOneFieldForward();
 
     Serial.println("\n=== TEST DONE ===");
     Serial.println("Sleeping for 10 seconds...\n");
@@ -44,15 +46,25 @@ void testWhitePawn() {
     // valid moves
     Serial.println("Testing valid moves...");
     ECPChessField validFields[] = {
-        { A, 3}, { A, 4 },
-        { B, 3 }};  // assuming that there is an opponent to capture
+        { A, 4 },
+        { A, 3 }, { B, 3 }  // assuming that there is an opponent to capture
+        /* fig */ };
     for (ECPChessField field : validFields) {
         test(pawn, field, true);
     }
     
     // invalid moves
     Serial.println("\nTesting invalid moves...");
-    ECPChessField invalidFields[] = {initialField, { A, 1 }, { A, 5 }, { B, 2 }, { B, 1}};
+    ECPChessField invalidFields[] = {
+        initialField,
+        { A, 8 }, { B, 8 }, { C, 8 }, { D, 8 }, { E, 8 }, { F, 8 }, { G, 8 }, { H, 8 },
+        { A, 7 }, { B, 7 }, { C, 7 }, { D, 7 }, { E, 7 }, { F, 7 }, { G, 7 }, { H, 7 },
+        { A, 6 }, { B, 6 }, { C, 6 }, { D, 6 }, { E, 6 }, { F, 6 }, { G, 6 }, { H, 6 },
+        { A, 5 }, { B, 5 }, { C, 5 }, { D, 5 }, { E, 5 }, { F, 5 }, { G, 5 }, { H, 5 },
+                  { B, 4 }, { C, 4 }, { D, 4 }, { E, 4 }, { F, 4 }, { G, 4 }, { H, 4 },
+                            { C, 3 }, { D, 3 }, { E, 3 }, { F, 3 }, { G, 3 }, { H, 3 },
+        /* fig */ { B, 2 }, { C, 2 }, { D, 2 }, { E, 2 }, { F, 2 }, { G, 2 }, { H, 2 },
+        { A, 1 }, { B, 1 }, { C, 1 }, { D, 1 }, { E, 1 }, { F, 1 }, { G, 1 }, { H, 1 }};
     for (ECPChessField field : invalidFields) {
         test(pawn, field, false);
     }
@@ -65,15 +77,50 @@ void testBlackPawn() {
     // valid moves
     Serial.println("Testing valid moves...");
     ECPChessField validFields[] = {
-        { C, 6}, { C, 5 },
-        { D, 6 }};  // assuming that there is an opponent to capture
+                  /* fig */ 
+        { B, 6 }, { C, 6 }, { D, 6 },   // assuming that there is an opponent to capture
+                  { C, 5 }};
     for (ECPChessField field : validFields) {
         test(pawn, field, true);
     }
 
     // invalid moves
     Serial.println("\nTesting invalid moves...");
-    ECPChessField invalidFields[] = {initialField, { C, 1 }, { C, 4 }, { B, 7 }, { D, 8 }};
+    ECPChessField invalidFields[] = {
+        initialField,
+        { A, 8 }, { B, 8 }, { C, 8 }, { D, 8 }, { E, 8 }, { F, 8 }, { G, 8 }, { H, 8 },
+        { A, 7 }, { B, 7 }, /* fig */ { D, 7 }, { E, 7 }, { F, 7 }, { G, 7 }, { H, 7 },
+        { A, 6 },                               { E, 6 }, { F, 6 }, { G, 6 }, { H, 6 },
+        { A, 5 }, { B, 5 },           { D, 5 }, { E, 5 }, { F, 5 }, { G, 5 }, { H, 5 },
+        { A, 4 }, { B, 4 }, { C, 4 }, { D, 4 }, { E, 4 }, { F, 4 }, { G, 4 }, { H, 4 },
+        { A, 3 }, { B, 3 }, { C, 3 }, { D, 3 }, { E, 3 }, { F, 3 }, { G, 3 }, { H, 3 },
+        { A, 2 }, { B, 2 }, { C, 2 }, { D, 2 }, { E, 2 }, { F, 2 }, { G, 2 }, { H, 2 },
+        { A, 1 }, { B, 1 }, { C, 1 }, { D, 1 }, { E, 1 }, { F, 1 }, { G, 1 }, { H, 1 }};
+    for (ECPChessField field : invalidFields) {
+        test(pawn, field, false);
+    }
+}
+
+void testPawnInBoardCenterCanOnlyMoveOneFieldForward() {
+    const ECPChessField initialField = { D, 5 };
+    ECPPawn pawn = ECPPawn(dezibot, initialField, true);
+
+    // valid moves
+    Serial.println("Testing valid moves...");
+    ECPChessField validFields[] = {
+        { D, 6 },
+        /* fig */
+    };
+    for (ECPChessField field : validFields) {
+        test(pawn, field, true);
+    }
+
+    // invalid moves
+    Serial.println("\nTesting invalid moves...");
+    ECPChessField invalidFields[] = { initialField,
+        { D, 7 }    // not allowed to move two fields if not on initial row
+        // D6 is valid
+        /* fig */};
     for (ECPChessField field : invalidFields) {
         test(pawn, field, false);
     }
--- src/chess_piece_move_validation_tests/rook/rook.ino
+++ src/chess_piece_move_validation_tests/rook/rook.ino
@@ -43,14 +43,31 @@ void testRookOnInitialField() {
 
     // valid moves
     Serial.println("\nTesting valid moves...");
-    ECPChessField validFields[] = {{ A, 2 }, { A, 8 }, { B, 1 }, { H, 1 }};
+    ECPChessField validFields[] = {
+        { A, 8 },
+        { A, 7 },
+        { A, 6 },
+        { A, 5 },
+        { A, 4 },
+        { A, 3 },
+        { A, 2 },
+        /* fig */ { B, 1 }, { C, 1 }, { D, 1 }, { E, 1 }, { F, 1 }, { G, 1 }, { H, 1 }};
     for (ECPChessField field : validFields) {
         test(rook, field, true);
     }
 
     // invalid moves
     Serial.println("\nTesting invalid moves...");
-    ECPChessField invalidFields[] = {initialField, { B, 4 }, { B, 8 }, { H, 8 }};
+    ECPChessField invalidFields[] = {
+        initialField,
+                  { B, 8 }, { C, 8 }, { D, 8 }, { E, 8 }, { F, 8 }, { G, 8 }, { H, 8 },
+                  { B, 7 }, { C, 7 }, { D, 7 }, { E, 7 }, { F, 7 }, { G, 7 }, { H, 7 },
+                  { B, 6 }, { C, 6 }, { D, 6 }, { E, 6 }, { F, 6 }, { G, 6 }, { H, 6 },
+                  { B, 5 }, { C, 5 }, { D, 5 }, { E, 5 }, { F, 5 }, { G, 5 }, { H, 5 },
+                  { B, 4 }, { C, 4 }, { D, 4 }, { E, 4 }, { F, 4 }, { G, 4 }, { H, 4 },
+                  { B, 3 }, { C, 3 }, { D, 3 }, { E, 3 }, { F, 3 }, { G, 3 }, { H, 3 },
+                  { B, 2 }, { C, 2 }, { D, 2 }, { E, 2 }, { F, 2 }, { G, 2 }, { H, 2 },
+        /* fig */ };
     for (ECPChessField field : invalidFields) {
         test(rook, field, false);
     }
@@ -60,26 +77,33 @@ void testRookInBoardCenter() {
     const ECPChessField initialField = { D, 5 };
     ECPRook rook = ECPRook(dezibot, initialField, false);
 
-    // valid moves - horizontally
-    Serial.println("\nTesting valid horizontal moves...");
-    ECPChessField validHorizontalFields[] = {{ A, 5 }, { B, 5 }, { C, 5 },
-        { E, 5 }, { F, 5 }, { G, 5 }, { H, 5 }};
+    // valid moves
+    Serial.println("\nTesting valid moves...");
+    ECPChessField validHorizontalFields[] = {
+                                      { D, 8 },
+                                      { D, 7 },
+                                      { D, 6 },
+        { A, 5 }, { B, 5 }, { C, 5 }, /* fig */ { E, 5 }, { F, 5 }, { G, 5 }, { H, 5 },
+                                      { D, 4 },
+                                      { D, 3 },
+                                      { D, 2 },
+                                      { D, 1 }};
     for (ECPChessField field : validHorizontalFields) {
-        test(rook, field, true);
-    }
-
-    // valid moves - vertically
-    Serial.println("\nTesting valid vertical moves...");
-    ECPChessField validVerticalFields[] = {{ D, 1 }, { D, 2 }, { D, 3 },
-        { D, 4 }, { D, 6 }, { D, 7 }, { D, 8 }};
-    for (ECPChessField field : validVerticalFields) {
         test(rook, field, true);
     }
 
     // invalid moves
     Serial.println("\nTesting invalid moves...");
-    ECPChessField invalidFields[] = {initialField, { D, 5 }, { C, 4 }, { C, 6 },
-        { E, 6 }, { E, 4 }};
+    ECPChessField invalidFields[] = {
+        initialField,
+        { A, 8 }, { B, 8 }, { C, 8 },           { E, 8 }, { F, 8 }, { G, 8 }, { H, 8 },
+        { A, 7 }, { B, 7 }, { C, 7 },           { E, 7 }, { F, 7 }, { G, 7 }, { H, 7 },
+        { A, 6 }, { B, 6 }, { C, 6 },           { E, 6 }, { F, 6 }, { G, 6 }, { H, 6 },
+                                      /* fig */
+        { A, 4 }, { B, 4 }, { C, 4 },           { E, 4 }, { F, 4 }, { G, 4 }, { H, 4 },
+        { A, 3 }, { B, 3 }, { C, 3 },           { E, 3 }, { F, 3 }, { G, 3 }, { H, 3 },
+        { A, 2 }, { B, 2 }, { C, 2 },           { E, 2 }, { F, 2 }, { G, 2 }, { H, 2 },
+        { A, 1 }, { B, 1 }, { C, 1 },           { E, 1 }, { F, 1 }, { G, 1 }, { H, 1 }};
     for (ECPChessField field : invalidFields) {
         test(rook, field, false);
     }
