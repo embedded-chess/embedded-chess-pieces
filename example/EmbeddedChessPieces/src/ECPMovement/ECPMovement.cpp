b44eb8c315b5a6a1861492d04f394711f8e2ce64
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-02-23 13:20:36 +0100

    Sorted functions according to visibility.

--- example/EmbeddedChessPieces/src/ECPMovement/ECPMovement.cpp
+++ example/EmbeddedChessPieces/src/ECPMovement/ECPMovement.cpp
@@ -8,23 +8,6 @@ ECPMovement::ECPMovement(
     ecpSignalDetection(ECPSignalDetection(dezibot)),
     movementCalibration(movementCalibration) {};
 
-void ECPMovement::moveForward(int timeMovement, int timeBreak) {
-    dezibot.motion.move(0, movementCalibration);
-    delay(timeMovement);
-    dezibot.motion.stop();
-    delay(timeBreak);
-};
-
-void ECPMovement::moveToNextField() {
-    const bool hasStartedOnWhite = ecpColorDetection.isWhiteField();
-    bool isCurrentlyOnWhite = hasStartedOnWhite;
-
-    while (isCurrentlyOnWhite == hasStartedOnWhite) {
-        moveForward(FORWARD_TIME, MOVEMENT_BREAK);
-        isCurrentlyOnWhite = ecpColorDetection.isWhiteField();
-    }
-};
-
 void ECPMovement::move(uint numberOfFields) {
     for (size_t i = 0; i < numberOfFields; i++) {
         moveToNextField();
@@ -74,6 +57,27 @@ void ECPMovement::turnRight(
     bool isCurrentlyOnWhite = ecpColorDetection.isWhiteField();
     if (isCurrentlyOnWhite != hasStartedOnWhite) {
         displayRotionCorrectionRequest(currentField, intendedDirection);
+    }
+};
+
+// -----------------------------------------------------------------------------
+// PRIVATE FUNCTIONS
+// -----------------------------------------------------------------------------
+
+void ECPMovement::moveForward(int timeMovement, int timeBreak) {
+    dezibot.motion.move(0, movementCalibration);
+    delay(timeMovement);
+    dezibot.motion.stop();
+    delay(timeBreak);
+};
+
+void ECPMovement::moveToNextField() {
+    const bool hasStartedOnWhite = ecpColorDetection.isWhiteField();
+    bool isCurrentlyOnWhite = hasStartedOnWhite;
+
+    while (isCurrentlyOnWhite == hasStartedOnWhite) {
+        moveForward(FORWARD_TIME, MOVEMENT_BREAK);
+        isCurrentlyOnWhite = ecpColorDetection.isWhiteField();
     }
 };
 
