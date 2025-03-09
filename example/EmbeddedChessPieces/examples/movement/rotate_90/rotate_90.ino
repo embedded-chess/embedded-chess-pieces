2018cbca9f54aafa7cf1f2ac7cea3a33cfaf246f
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-03-09 19:36:28 +0100

    Remove unnecessary serial prints in rotate 90 sketch.

--- example/EmbeddedChessPieces/examples/movement/rotate_90/rotate_90.ino
+++ example/EmbeddedChessPieces/examples/movement/rotate_90/rotate_90.ino
@@ -11,7 +11,6 @@
 
 #include <Dezibot.h>
 #include <EmbeddedChessPieces.h>
-#include <Wire.h>
 
 // change for a calibration fitting the specific dezibot
 #define MOVEMENT_CALIBRATION 3900
@@ -20,24 +19,22 @@ Dezibot dezibot = Dezibot();
 ECPMovement ecpMovement(dezibot, MOVEMENT_CALIBRATION);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Started");
   dezibot.begin();
-  Serial.println("Initialised");
+  dezibot.display.flipOrientation();
   delay(500);
 }
 
 void loop() {
   dezibot.display.println("Turning left...");
-  ecpMovement.turnLeft({A, 1}, NORTH);
+  ecpMovement.turnLeft({A, 1}, WEST);
   dezibot.display.println("Done");
-  delay(2000);
+  delay(10000);
 
   dezibot.display.println("Turning right...");
   ecpMovement.turnRight({A, 1}, NORTH);
   dezibot.display.println("Done");
   
-  dezibot.display.println("\nSleeping for 10s...");
+  dezibot.display.println("\Sleep 10s");
   delay(10000);
   dezibot.display.clear();
 }
